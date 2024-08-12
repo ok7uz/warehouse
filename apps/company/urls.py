5c1b4467e87f45b4dfc5e24f5071f3bb9328001e
komronbek <komronbekobloev@gmail.com>
2024-08-12 12:47:04 +0500

    Added gunicorn

--- apps/company/urls.py
+++ apps/company/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from apps.company.views import *
 
 urlpatterns = [
-    path('company/', CompanyListView.as_view(), name='company-list'),
-    path('company/<uuid:company_id>/sales/', CompanySalesView.as_view(), name='company-sales'),
-    path('company/<uuid:uuid>/', CompanyDetailView.as_view(), name='company-update'),
+    path('companies/', CompanyListView.as_view(), name='company-list'),
+    path('companies/<uuid:company_id>/sales/', CompanySalesView.as_view(), name='company-sales'),
+    path('companies/<uuid:uuid>/', CompanyDetailView.as_view(), name='company-update'),
 ]
