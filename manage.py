a81d70533e6d3bd629a21ddfe3a1fcf764483149
anasazamov <anasazamov55@gmail.com>
2024-08-26 18:42:31 +0500

    view database name

--- manage.py
+++ manage.py
@@ -2,7 +2,10 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from decouple import config
 
+print(config("DB_NAME"))
+sys.exit()
 
 def main():
     """Run administrative tasks."""
