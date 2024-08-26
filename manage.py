b688e5d2dc8b25c331159b898d5e7e8769443f31
anasazamov <anasazamov55@gmail.com>
2024-08-26 18:47:17 +0500

    delete view database

--- manage.py
+++ manage.py
@@ -4,9 +4,6 @@ import os
 import sys
 from decouple import config
 
-print(config("DB_NAME"))
-sys.exit()
-
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.development')
